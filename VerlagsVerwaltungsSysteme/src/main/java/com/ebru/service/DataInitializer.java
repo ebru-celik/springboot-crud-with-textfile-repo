0c89d865153f52029230d4ba8d3f8f0bcb711a2d
ebru-celik <ebrucelikcinar@gmail.com>
2024-04-28 14:04:13 +0200

    ConcurrentMap
    added concurrentMap to collect data from file and To reduce continuous file reading operations

--- VerlagsVerwaltungsSysteme/src/main/java/com/ebru/service/DataInitializer.java
+++ VerlagsVerwaltungsSysteme/src/main/java/com/ebru/service/DataInitializer.java
@@ -4,6 +4,7 @@ import com.ebru.model.Autor;
 import com.ebru.model.Buch;
 import com.ebru.model.Verlag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +13,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
-public class DataInitializer {
+public class DataInitializer implements InitializingBean {
 
     @Value("${verlag.file.path}")
     private String verlagFilePath;
@@ -29,6 +32,16 @@ public class DataInitializer {
     @Value("${autor.file.path}")
     private String autorFilePath;
 
+    private ConcurrentMap<Long, Autor> autoren_map = new ConcurrentHashMap<>();
+    private ConcurrentMap<Long, Buch> buch_map = new ConcurrentHashMap<>();
+    private ConcurrentMap<Long, Verlag> verlag_map = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        loadVerlageFromFile().forEach(obj->verlag_map.put(obj.getVerlagID(), obj));
+        loadBuecherFromfile().forEach(obj->buch_map.put(obj.getBuchID(),obj));
+        loadAutorenFromFile().forEach(obj-> autoren_map.put(obj.getAutorID(), obj));
+    }
 
     public <T> List<T> loadObjects(String filePath, Function<String[], T> mapper)  {
         try(Stream<String> lines = Files.lines(Paths.get(filePath))){
@@ -41,52 +54,37 @@ public class DataInitializer {
         }
     }
 
-    public List<Verlag> loadVerlage()  {
+    private List<Verlag> loadVerlageFromFile()  {
         return loadObjects(verlagFilePath, data -> new Verlag(Long.parseLong(data[0]), data[1]));
     }
-
-    public List<Buch> loadBuecher() {
+    private List<Buch> loadBuecherFromfile() {
         return loadObjects(buchFilePath, data -> new Buch(Long.parseLong(data[0]), data[1], Double.parseDouble(data[2]), data[3], Long.parseLong(data[4])));
     }
-
-    public List<Autor> loadAutoren() {
+    private List<Autor> loadAutorenFromFile() {
         return loadObjects(autorFilePath, data -> new Autor(Long.parseLong(data[0]), data[1], data[2], Long.parseLong(data[3])));
     }
 
+    public List<Verlag> loadVerlage()  {
+        return verlag_map.values().stream().toList();
+    }
+    public List<Buch> loadBuecher() {
+        return buch_map.values().stream().toList();
+    }
+    public List<Autor> loadAutoren() {
+        return autoren_map.values().stream().toList();
+    }
+
     public List<Buch> findBuecherByVerlagId(Long verlagId){
-        try(Stream<String> buchLines = Files.lines(Paths.get(buchFilePath))){
-            return buchLines
-                    .map(line -> line.split(";"))
-                    .map(data -> new Buch(Long.parseLong(data[0]), data[1], Double.parseDouble(data[2]), data[3], Long.parseLong(data[4])))
-                    .filter(data -> data.getVerlagID().equals(verlagId))
-                    .toList();
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return buch_map.values().stream().filter(data -> data.getVerlagID().equals(verlagId)).toList();
     }
 
     public List<Autor>findAutorenByBuchId(Long buchId){
-        try(Stream<String> lines = Files.lines(Paths.get(autorFilePath))) {
-            return lines
-                    .map(line -> line.split(";"))
-                    .map(data -> new Autor(Long.parseLong(data[0]), data[1], data[2], Long.parseLong(data[3])))
-                    .filter(data -> data.getBuchID().equals(buchId))
-                    .collect(Collectors.toList());
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return autoren_map.values().stream().filter(data -> data.getBuchID().equals(buchId)).toList();
     }
 
     public Optional<Verlag> findVerlagByVerlagId(Long verlagId){
-        try(Stream<String> lines = Files.lines(Paths.get(verlagFilePath))){
-            return lines
-                    .map(line -> line.split(";"))
-                    .map(data -> new Verlag(Long.parseLong(data[0]), data[1]))
-                    .filter(data->data.getVerlagID().equals(verlagId))
-                    .findFirst();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return verlag_map.values().stream().filter(data->data.getVerlagID().equals(verlagId)).findFirst();
     }
+
 
 }
